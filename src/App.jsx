88af3f2278c503dce8df8d7873dea84e55550e12
Pablo Marino <pmarino2013@gmail.com>
2025-03-11 21:31:20 -0300

    primer componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import React from "react";
+import ParrafoApp from "./components/ParrafoApp";
+
 const App = () => {
-  let activado = true;
   return (
     <div>
-      {activado ? <h1>Hola mundo!!</h1> : <h1>No es un buen día</h1>}
-
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dicta pariatur, tempora quas recusandae autem ea dolorem? Animi nemo,
-        iste, quasi mollitia libero culpa quam, harum ratione aspernatur fugit
-        inventore.
-      </p>
+      <h1>Soy mi primer componente</h1>
+      <ParrafoApp />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,98 +1,98 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-import { saludarAlumnos } from "./funciones.js";
-import persona from "./data.js";
+// import { saludarAlumnos } from "./funciones.js";
+// import persona from "./data.js";
 
-//Desestructuración
-const elementos = ["Hola mundo", 45, () => console.log("Chau!!")];
+// //Desestructuración
+// const elementos = ["Hola mundo", 45, () => console.log("Chau!!")];
 
-const [palabra, numerito, saludar] = elementos;
+// const [palabra, numerito, saludar] = elementos;
 
-console.log(palabra);
-console.log(numerito);
+// console.log(palabra);
+// console.log(numerito);
 
-console.log(`Mi número favorito es ${numerito}`);
-saludar();
+// console.log(`Mi número favorito es ${numerito}`);
+// saludar();
 
-let usuario = {
-  id: 1,
-  name: "Leanne Graham",
-  username: "Bret",
-  email: "Sincere@april.biz",
-  address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    geo: {
-      lat: "-37.3159",
-      lng: "81.1496",
-    },
-  },
-  phone: "1-770-736-8031 x56442",
-  website: "hildegard.org",
-  company: {
-    name: "Romaguera-Crona",
-    catchPhrase: "Multi-layered client-server neural-net",
-    bs: "harness real-time e-markets",
-  },
-};
-
-console.log(usuario.address.geo.lat);
-console.log(usuario.address.geo.lng);
-
-const { lat, lng } = usuario.address.geo;
-const { name, email } = usuario;
-// const {
+// let usuario = {
+//   id: 1,
+//   name: "Leanne Graham",
+//   username: "Bret",
+//   email: "Sincere@april.biz",
 //   address: {
-//     geo: { lat, lng },
+//     street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874",
+//     geo: {
+//       lat: "-37.3159",
+//       lng: "81.1496",
+//     },
 //   },
-// } = usuario;
-console.log(name);
-console.log(email);
-console.log(lat);
-console.log(lng);
+//   phone: "1-770-736-8031 x56442",
+//   website: "hildegard.org",
+//   company: {
+//     name: "Romaguera-Crona",
+//     catchPhrase: "Multi-layered client-server neural-net",
+//     bs: "harness real-time e-markets",
+//   },
+// };
 
-//Operador ternario
-let activado = true;
-// if (activado) {
-//   console.log("Sistema online");
-// } else {
-//   console.error("Sistema offline");
-// }
+// console.log(usuario.address.geo.lat);
+// console.log(usuario.address.geo.lng);
 
-activado ? console.log("Sistema online") : console.error("Sistema offline");
+// const { lat, lng } = usuario.address.geo;
+// const { name, email } = usuario;
+// // const {
+// //   address: {
+// //     geo: { lat, lng },
+// //   },
+// // } = usuario;
+// console.log(name);
+// console.log(email);
+// console.log(lat);
+// console.log(lng);
 
-activado && console.log("Sistema online");
+// //Operador ternario
+// let activado = true;
+// // if (activado) {
+// //   console.log("Sistema online");
+// // } else {
+// //   console.error("Sistema offline");
+// // }
 
-//Spread operator
-const frutas = ["🍎", "🍌"];
-const copiaFrutas = [...frutas];
+// activado ? console.log("Sistema online") : console.error("Sistema offline");
 
-copiaFrutas.push("🍐");
-console.log(frutas);
-console.log(copiaFrutas);
+// activado && console.log("Sistema online");
 
-const verduras = ["🥦", "🥕"];
+// //Spread operator
+// const frutas = ["🍎", "🍌"];
+// const copiaFrutas = [...frutas];
 
-const comida = [...frutas, ...verduras];
-console.log(comida);
+// copiaFrutas.push("🍐");
+// console.log(frutas);
+// console.log(copiaFrutas);
 
-const numeritos = [5, 6, 2, 34];
-console.log(Math.max(...numeritos)); //5, 6, 2, 34
+// const verduras = ["🥦", "🥕"];
 
-let frase = "La vida es bella";
-const frasesArray = [...frase];
-console.log(frasesArray);
+// const comida = [...frutas, ...verduras];
+// console.log(comida);
 
-saludarAlumnos();
-console.log(persona);
+// const numeritos = [5, 6, 2, 34];
+// console.log(Math.max(...numeritos)); //5, 6, 2, 34
+
+// let frase = "La vida es bella";
+// const frasesArray = [...frase];
+// console.log(frasesArray);
+
+// saludarAlumnos();
+// console.log(persona);
